4586cafc6c4aef4d6360c0eb165c1eff94813058
Chandradeep-Pra <chandradeepprasad1998@gmail.com>
2024-09-23 00:27:39 +0530

    normal

--- components/shared/navbar/Mobilenav.tsx
+++ components/shared/navbar/Mobilenav.tsx
@@ -1,45 +1,76 @@
-import React from 'react'
+import React from "react";
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
-import Image from 'next/image'
-import Link from 'next/link'
-  
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
-const  Mobilenav = () => {
+const NavContent = () => {
+  return <h1 className="text-3xl">Nav Content</h1>;
+};
+
+const Mobilenav = () => {
   return (
     <Sheet>
-  <SheetTrigger asChild>
-    <Image 
-        src='/assets/icons/hamburger.svg'
-        alt='menu'
-        width={36}
-        height={36}
-        className='invert-colors sm:hidden'
-    />
-  </SheetTrigger>
-  <SheetContent side='left' className='background-light900_dark200 border-none'>
-  <Link href="/" className='flex items-center gap-1'>
-            <Image 
-                src="/assets/images/site-logo.svg"
-                width={23}
-                height={23}
-                alt='codeflow'
-            />
-            <p className='h2-bold font-spaceGrotesk text-dark100_light900'>Code <span className='text-primary-500'> Flow</span></p>
-    </Link>
-    <div>
-        
-    </div>
-  </SheetContent>
-</Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          alt="menu"
+          width={36}
+          height={36}
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="codeflow"
+          />
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
+            Code <span className="text-primary-500"> Flow</span>
+          </p>
+        </Link>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+          <SignedOut>
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href="/sign-in">
+                  <Button className="small-medium btn-secondary min-h[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient font-bold ">
+                      Log In
+                    </span>
+                  </Button>
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="small-medium btn-secondary min-h[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient font-bold ">
+                      Sign Up
+                    </span>
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-  )
-}
-
-export default  Mobilenav
+export default Mobilenav;
