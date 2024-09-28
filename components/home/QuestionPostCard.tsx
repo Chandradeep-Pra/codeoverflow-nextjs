df4b8ea3f503d274c70c3cedcf08888ac39bf4ad
Chandradeep-Pra <chandradeepprasad1998@gmail.com>
2024-09-28 20:03:31 +0530

    Minor typescript and eslint bug fix

--- components/home/QuestionPostCard.tsx
+++ components/home/QuestionPostCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Rendertags from "../shared/Rendertags";
-import Image from "next/image";
+// import Image from "next/image";  
 import Link from "next/link";
 import Metrics from "../shared/Metrics";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
